5644ed48e64bbfb306ed0a74d4a70af4841de128
John O'Connor <oconnojb@gmail.com>
2018-07-28 18:54:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ function findMatching(collection, string){
   const newCollection = [];
 
   for (const item of collection) {
-    for (var i = 0; i < item.length; i++) {
+    for (var i = 0; i < collection.length; i++) {
       if (item[i] === string) {
         newCollection.push(item[i])
       }
     }
   }
- 
+
   return newCollection;
 }
