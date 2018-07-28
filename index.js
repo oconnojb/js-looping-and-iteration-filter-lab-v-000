c6a3370c1e34b3de9e639b518be2c4815676aff0
John O'Connor <oconnojb@gmail.com>
2018-07-28 18:52:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 // Code your solution in this file
-function findMatching(){
-  
+function findMatching(collection, string){
+  const newCollection = [];
+
+  for (const item of collection) {
+    for (var i = 0; i < item.length; i++) {
+      if (item[i] === string) {
+        newCollection.push(item[i])
+      }
+    }
+  }
+ 
+  return newCollection;
 }
